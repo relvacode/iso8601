c611dd6068f4d4f4e7ed27ca6270c6e03950ebcd
Jason Kingsbury <jason@relva.co.uk>
2021-05-07 12:14:49 +0100

    simplify daysIn by using daysInMonth array

--- time.go
+++ time.go
@@ -9,28 +9,12 @@ func isLeap(year int) bool {
 	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
 
-// daysBefore[m] counts the number of days in a non-leap year
-// before month m begins. There is an entry for m=12, counting
-// the number of days before January of next year (365).
-var daysBefore = [...]int32{
-	0,
-	31,
-	31 + 28,
-	31 + 28 + 31,
-	31 + 28 + 31 + 30,
-	31 + 28 + 31 + 30 + 31,
-	31 + 28 + 31 + 30 + 31 + 30,
-	31 + 28 + 31 + 30 + 31 + 30 + 31,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30,
-	31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31,
-}
+// daysInMonth is the number of days for non-leap years in each calendar month starting at 1
+var daysInMonth = [...]int{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
 func daysIn(m time.Month, year int) int {
 	if m == time.February && isLeap(year) {
 		return 29
 	}
-	return int(daysBefore[m] - daysBefore[m-1])
+	return daysInMonth[int(m)]
 }

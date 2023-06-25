07b2d7917250634403cc2619f69cc58672823d10
Chun-Hung Tseng <henrybear327@gmail.com>
2023-06-25 10:57:58 +0200

    Replace ISO8061 typos with ISO8601
    Signed-off-by: Chun-Hung Tseng <henrybear327@gmail.com>

--- iso8601.go
+++ iso8601.go
@@ -24,7 +24,7 @@ const (
 	charStart uint = '0'
 )
 
-// ParseISOZone parses the 5 character zone information in an ISO8061 date string.
+// ParseISOZone parses the 5 character zone information in an ISO8601 date string.
 // This function expects input that matches:
 //
 //	-0100
